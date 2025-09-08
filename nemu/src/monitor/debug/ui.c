0162af0c95ee20a2963ad908c917a7a11d3075eb
tracer-NEMU2020 <tjuics2020@163.com>
2025-09-08 11:54:31 +0000

    >  compile NEMU 3024244405 stardream Linux b7256102dd99 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  11:54:31 up  2:30,  0 users,  load average: 0.02, 0.02, 0.00 16a7eb3037db6e05464339e0bef33d714ea2cbba

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -70,24 +70,30 @@ static int cmd_info(char *args) {	//打印寄存器状态
 }
 
 static int cmd_x(char *args) {
-  /* 1. 取 N */
   char *sN = strtok(args, " ");
-  if (!sN) return 0;
-  int N = atoi(sN);
+  if (!sN) 
+  	return 0;
+  int N = atoi(sN);               
 
-  /* 2. 取十六进制字符串并解析 */
   char *hexStr = strtok(NULL, " ");
-  if (!hexStr) return 0;
+  if (!hexStr) 
+  	return 0;
+	
   uint32_t addr = (uint32_t)strtol(hexStr, NULL, 16);
 
-  /* 3. 一次读 4 字节，共 N 次 */
   int i;
   for (i = 0; i < N; ++i) {
-    uint32_t data = swaddr_read(addr + i * 4, 4);
-    printf("0x%08x\n", data);
+    if (i % 4 == 0)               
+      printf("0x%08x: ", addr + i * 4);
+      printf("0x%08x ", swaddr_read(addr + i * 4, 4));
+
+    if ((i + 1) % 4 == 0) puts("");   
   }
+  if (N % 4) 
+  	puts("");                
   return 0;
 }
+
 static int cmd_help(char *args);
 
 static struct {
